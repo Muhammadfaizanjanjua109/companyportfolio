13bc3e6cc279b402e75bfb4f809a2a51ddbdc5c4
AfzaalOther <afzaal.other@gmail.com>
2024-08-15 14:02:16 +0500

    Update Footer.js
    email updated

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
               <div className="footer-links">
                 <h4>Contact Us</h4>
                 <p>
-                   http://info.aicdf.au
+                   info.aicdf.au
                  </p>
               </div>
             </div>
